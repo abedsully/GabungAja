3b2d2614586aeb1afdccf2e21d867ee017cfc82f
abedyusop0 <stefanus.wilson@binus.ac.id>
2024-04-18 23:52:36 +0700

    feat: ui for login, register, navbar, and footer

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('authentication.register');
+});
+
+Route::get('/login', function () {
+    return view('authentication.login');
+});
